f19b642af581f204086048f1a698669c85f63dba
Maximo Crupi <maximocrupi@Maximos-MacBook-Pro.local>
2019-08-29 07:39:04 -0600

    first commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Maximo Crupi on 8/26/19.
+//  Copyright © 2019 Maximo Crupi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
